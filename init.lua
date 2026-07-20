604770c6a25b01e056e9ee12790c1ced61fc8023
Bradley Wood <52896759+BradleyLWood@users.noreplay.github.com>
2026-07-20 12:21:01 -0600

    added more configuration to init.lua

--- init.lua
+++ init.lua
@@ -1,55 +1,90 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
---vim.opt.scrolloff = 10
-
-local scrolloff = math.floor(vim.o.lines / 2) - 4
-vim.opt.scrolloff = scrolloff
-
-vim.opt.conceallevel = 1
+-- Plugins
 
 require("plugins.init")
 require("config.binds")
 
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd('packadd nvim.undotree')
+vim.keymap.set('n', '<leader>u', require('undotree').open)
 
-vim.opt.cursorline = true
-vim.wo.number = true
-vim.wo.relativenumber = true
+
+-- Colors
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("catppuccin-mocha")
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#6c7086" })
 vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#cba6f7", bold = true })
 
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.o.winborder = "rounded"
+-- Options
 
-vim.opt.inccommand = 'split'
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = false
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.wrap = false
+vim.wo.linebreak = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
-
-vim.opt.showmode = false
-
-vim.opt.cmdheight = 1
-
-vim.opt.wrap = false
-vim.wo.wrap = false
-vim.wo.linebreak = true
-
+vim.opt.expandtab = true
 vim.opt.autoindent = true
+vim.opt.smartindent = true
 vim.opt.breakindent = true
 
-vim.opt.updatetime = 100
-vim.opt.timeoutlen = 300
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.inccommand = 'split'
+vim.opt.hlsearch = false
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-vim.opt.list = false
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '120'
+vim.opt.showmatch = true
+vim.opt.cmdheight = 1
+vim.opt.completeopt = 'menuone,noinsert,noselect'
+vim.opt.showmode = false
+vim.opt.conceallevel = 2
+vim.opt.concealcursor = ''
+vim.opt.synmaxcol = 300
+vim.opt.fillchars = { eob = " " }
+
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 500
+vim.opt.ttimeoutlen = 50
+vim.opt.autoread = true
+vim.opt.autowrite = false
+
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.iskeyword:append('-')
+vim.opt.path:append('**')
+vim.opt.selection = 'inclusive'
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.o.winborder = 'rounded'
+
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'longest:full,full'
+vim.opt.diffopt:append('linematch:60')
+vim.opt.redrawtime = 10000
+vim.opt.maxmempattern = 20000
+
+lim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', extends = "⟩", precedes = "⟨" }
 
 vim.opt.termguicolors = true
@@ -58,12 +93,3 @@ vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 
 -- Allow selection of empty space in line block mode
 vim.opt.virtualedit = 'block'
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.cmd('packadd nvim.undotree')
-vim.keymap.set('n', '<leader>u', require('undotree').open)
-
