dabfa878b81e20a413218b6e6723f2401aaafae4
Bradley Wood <52896759+BradleyLWood@users.noreplay.github.com>
2026-07-26 16:35:11 -0600

    added markdown/obsidian and stylua format config for nvim lua files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,78 +1,79 @@
 vim.pack.add({
-	{ src = 'https://github.com/stevearc/conform.nvim', name = 'conform' },
-	{ src = 'https://github.com/mfussenegger/nvim-lint', name = 'lint' },
+    { src = 'https://github.com/stevearc/conform.nvim', name = 'conform' },
+    { src = 'https://github.com/mfussenegger/nvim-lint', name = 'lint' },
 })
 
 require('conform').setup({
-	formatters_by_ft = {
-		lua = { 'stylua' },
-		javascript = { 'prettier' },
-		typescriptreact = { 'prettier' },
-		typescript = { 'prettier' },
-		html = { 'prettier' },
-		python = { 'black' },
-		nix = { 'alejandra' },
-		css = { 'prettier' },
-		rust = { 'rustfmt' },
-	},
-  formatters = {
-    ocamlformat = {
-      prepend_args = {
-        '--if-then-else',
-        'vertical',
-        '--break-cases',
-        'fit-or-vertical',
-        '--type-decl',
-        'sparse',
-      },
+    formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        html = { 'prettier' },
+        python = { 'black' },
+        nix = { 'alejandra' },
+        css = { 'prettier' },
+        rust = { 'rustfmt' },
     },
-  },
-	format_on_save = false,
-	undojoin = true,
+    formatters = {
+        prettier = {
+            prepend_args = {
+                '--single-quote',
+            },
+        },
+        ocamlformat = {
+            prepend_args = {
+                '--if-then-else',
+                'vertical',
+                '--break-cases',
+                'fit-or-vertical',
+                '--type-decl',
+                'sparse',
+            },
+        },
+    },
+    undojoin = true,
 })
 
 local lint = require('lint')
 
 -- Only show diagnostics close to the cursor
 vim.diagnostic.config({
-	virtual_text = {
-		spacing = 4,
-		prefix = function(diagnostic)
-			local icons = {
-				[vim.diagnostic.severity.ERROR] = ' ',
-				[vim.diagnostic.severity.WARN] = '󰉀 ',
-				[vim.diagnostic.severity.INFO] = ' ',
-				[vim.diagnostic.severity.HINT] = '󰌵 ',
-			}
-			return icons[diagnostic.severity] or ''
-		end,
-	},
-	signs = false,
-	underline = true,
-	update_in_insert = false,
+    virtual_text = {
+        spacing = 4,
+        prefix = function(diagnostic)
+            local icons = {
+                [vim.diagnostic.severity.ERROR] = ' ',
+                [vim.diagnostic.severity.WARN] = '󰉀 ',
+                [vim.diagnostic.severity.INFO] = ' ',
+                [vim.diagnostic.severity.HINT] = '󰌵 ',
+            }
+            return icons[diagnostic.severity] or ''
+        end,
+    },
+    signs = false,
+    underline = true,
+    update_in_insert = false,
 })
 
 -- Auto-run the linter only for the configured filetypes
 vim.api.nvim_create_autocmd('BufWritePost', {
-	callback = function()
-		local ft = vim.bo.filetype
-		if lint.linters_by_ft[ft] then
-			lint.try_lint()
-		end
-	end,
+    callback = function()
+        local ft = vim.bo.filetype
+        if lint.linters_by_ft[ft] then
+            lint.try_lint()
+        end
+    end,
 })
 
-
 local function map(mode, key, binding, opts)
-	local options = { noremap = true, silent = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.keymap.set(mode, key, binding, options)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, key, binding, options)
 end
 
-map('n', '<leader>f',
-  function() require('conform').format({ async = true, lsp_fallback = true }) end,
-	{ desc = 'Format buffer'}
-)
-
+map('n', '<leader>f', function()
+    require('conform').format({ async = true, lsp_fallback = true })
+end, { desc = 'Format buffer' })
---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,48 @@
+vim.pack.add({
+    { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim', name = 'markdown' },
+    {
+        src = 'https://github.com/obsidian-nvim/obsidian.nvim',
+        version = vim.version.range('*'), -- use latest release, remove to use latest commit
+    },
+})
+
+require('render-markdown').setup({
+    completions = { lsp = { enabled = true } },
+    max_file_size = 10.0,
+    debounce = 200,
+    checkbox = {
+        enabled = false,
+        bullet = false,
+        --right_pad = 1,
+        unchecked = { icon = '☐' },
+        checked = { icon = '☑' },
+    },
+    bullet = {
+        left_pad = 2,
+        right_pad = 1,
+    },
+})
+
+local obsidian = require('obsidian')
+obsidian.setup({
+    workspaces = {
+        {
+            name = 'vault',
+            path = '~/notes/vault',
+        },
+        {
+            name = 'work',
+            path = '~/notes/work',
+        },
+    },
+    templates = {
+        folder = 'Templates',
+        --date_format = '%Y-%m-%d-%a',
+        time_format = '%H:%M',
+    },
+})
+obsidian.ui = { enable = false }
+--obsidian.event = {
+--    'BufReadPre ' .. vim.fn.expand('~') .. '/notes/*/*.md',
+--    'BufNewFile ' .. vim.fn.expand('~') .. '/notes/*/*.md',
+--}
