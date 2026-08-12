098749c54d4ce82863048a7bcbe3db517449482e
Bradley Wood <52896759+BradleyLWood@users.noreply.github.com>
2026-08-12 09:26:05 -0600

    improved blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,134 +1,165 @@
 vim.pack.add({
-	{ src = "https://github.com/neovim/nvim-lspconfig", name = "lspconfig" },
-	{ src = "https://github.com/saghen/blink.cmp", name = "blink" },
-	{ src = "https://github.com/saghen/blink.lib", name = "blink-lib" },
+    { src = 'https://github.com/neovim/nvim-lspconfig', name = 'lspconfig' },
+    { src = 'https://github.com/saghen/blink.cmp', name = 'blink' },
+    { src = 'https://github.com/saghen/blink.lib', name = 'blink-lib' },
 })
 
 local hostname = vim.uv.os_gethostname()
 
-vim.lsp.config("nixd", {
-	cmd = { "nixd" },
-	filetypes = { "nix" },
-	root_markers = { "flake.nix", ".git" },
-	settings = {
-		nixd = {
-			nixpkgs = {
-				expr = "import <nixpkgs> { }",
-			},
-			formatting = {
-				command = { "alejandra" },
-			},
-			options = {
-				nixos = {
-					expr = '(builtins.getFlake "~/.dotfiles/.nixos").nixosConfigurations.'
-						.. string.lower(hostname)
-						.. ".options",
-				},
-			},
-		},
-	},
+vim.lsp.config('nixd', {
+    cmd = { 'nixd' },
+    filetypes = { 'nix' },
+    root_markers = { 'flake.nix', '.git' },
+    settings = {
+        nixd = {
+            nixpkgs = {
+                expr = 'import <nixpkgs> { }',
+            },
+            formatting = {
+                command = { 'alejandra' },
+            },
+            options = {
+                nixos = {
+                    expr = '(builtins.getFlake "~/.dotfiles/.nixos").nixosConfigurations.'
+                        .. string.lower(hostname)
+                        .. '.options',
+                },
+            },
+        },
+    },
 })
 
-vim.lsp.config("lua_ls", {
-	settings = {
-		Lua = {
-			-- Tell the server to let Neovim handle snippet expansion
-			completion = {
-				callSnippet = "Replace",
-			},
-			-- Use LuaJIT (which Neovim uses)
-			runtime = {
-				version = "LuaJIT",
-			},
-		},
-	},
+vim.lsp.config('lua_ls', {
+    settings = {
+        Lua = {
+            -- Tell the server to let Neovim handle snippet expansion
+            completion = {
+                callSnippet = 'Replace',
+            },
+            -- Use LuaJIT (which Neovim uses)
+            runtime = {
+                version = 'LuaJIT',
+            },
+        },
+    },
 })
 
 vim.lsp.enable({
-	"lua_ls",
-	"ts_ls",
-	"pylsp",
-	"cssls",
-	"svelte",
-	"nixd",
-	"rust_analyzer",
-	"emmet_language_server",
+    'lua_ls',
+    'ts_ls',
+    'pylsp',
+    'cssls',
+    'svelte',
+    'nixd',
+    'rust_analyzer',
+    'emmet_language_server',
 })
 
-vim.o.pumborder = "rounded"
+vim.o.pumborder = 'rounded'
 
-require("blink.cmp").setup({
+require('blink.cmp').setup({
 
-	fuzzy = { implementation = "lua" },
+    signature = {
+        enabled = true,
+    },
 
-	signature = {
-		enabled = true,
-	},
+    completion = {
+        trigger = {
+            show_on_insert = true,
+            show_on_trigger_character = true,
+            show_on_keyword = true,
+            show_on_backspace = true,
+        },
+        list = {
+            selection = {
+                preselect = false,
+                auto_insert = true,
+            },
+        },
+        menu = {
+            auto_show = true,
+            border = 'rounded',
+            min_width = 35,
+            auto_show_delay_ms = 100,
+            draw = {
+                columns = {
+                    { 'label', 'label_description', gap = 1 },
+                    { 'kind_icon', 'kind', gap = 2 },
+                },
+                components = {
+                    kind_icon = {
+                        text = function(ctx)
+                            local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                            return kind_icon
+                        end,
+                        -- (optional) use highlights from mini.icons
+                        highlight = function(ctx)
+                            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                            return hl
+                        end,
+                    },
+                    kind = {
+                        -- (optional) use highlights from mini.icons
+                        highlight = function(ctx)
+                            local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                            return hl
+                        end,
+                    },
+                },
+            },
+        },
+        documentation = {
+            window = {
+                border = 'rounded',
+            },
+        },
+    },
 
-	completion = {
-		trigger = {
-			show_on_insert = true,
-			show_on_trigger_character = true,
-			show_on_keyword = true,
-			show_on_backspace = true,
-		},
-		list = {
-			selection = {
-				preselect = false,
-				auto_insert = true,
-			},
-		},
-		menu = {
-			auto_show = true,
-			border = "rounded",
-			min_width = 35,
-			auto_show_delay_ms = 100,
-		},
-	},
+    fuzzy = { implementation = 'lua' },
 
-	-- Keymaps
-	keymap = {
-		["<C-u>"] = { "scroll_signature_up", "fallback" },
-		["<C-d>"] = { "scroll_signature_down", "fallback" },
-	},
-	sources = {
-		default = {
-			"lsp", -- (Equivalent to cmp-nvim-lsp)
-			"snippets", -- (Handled by the snippets config, replaces cmp_luasnip source)
-			"buffer", -- (Equivalent to cmp-buffer)
-			"path", -- (Equivalent to cmp-path)
-		},
-		providers = {
-			lazydev = {
-				name = "LazyDev",
-				module = "lazydev.integrations.blink",
-				-- make lazydev completions top priority (see `:h blink.cmp`)
-				score_offset = 100,
-			},
-		},
-	},
+    -- Keymaps
+    keymap = {
+        ['<C-u>'] = { 'scroll_signature_up', 'fallback' },
+        ['<C-d>'] = { 'scroll_signature_down', 'fallback' },
+    },
+    sources = {
+        default = {
+            'lsp', -- (Equivalent to cmp-nvim-lsp)
+            'snippets', -- (Handled by the snippets config, replaces cmp_luasnip source)
+            'buffer', -- (Equivalent to cmp-buffer)
+            'path', -- (Equivalent to cmp-path)
+        },
+        providers = {
+            lazydev = {
+                name = 'LazyDev',
+                module = 'lazydev.integrations.blink',
+                -- make lazydev completions top priority (see `:h blink.cmp`)
+                score_offset = 100,
+            },
+        },
+    },
 })
 
-vim.api.nvim_create_autocmd("FileType", { -- Lazy load lazydev when in lua file
-	pattern = "lua",
-	callback = function()
-		vim.pack.add({
-			{ src = "https://github.com/folke/lazydev.nvim", name = "lazydev" },
-		})
-		require("lazydev").setup()
-		require("blink.cmp").setup({ -- Reload blink with lazydev as a source
-			sources = {
-				-- add lazydev to your completion providers
-				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						-- make lazydev completions top priority (see `:h blink.cmp`)
-						score_offset = 100,
-					},
-				},
-			},
-		})
-	end,
+vim.api.nvim_create_autocmd('FileType', { -- Lazy load lazydev when in lua file
+    pattern = 'lua',
+    callback = function()
+        vim.pack.add({
+            { src = 'https://github.com/folke/lazydev.nvim', name = 'lazydev' },
+        })
+        require('lazydev').setup()
+        require('blink.cmp').setup({ -- Reload blink with lazydev as a source
+            sources = {
+                -- add lazydev to your completion providers
+                default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+                providers = {
+                    lazydev = {
+                        name = 'LazyDev',
+                        module = 'lazydev.integrations.blink',
+                        -- make lazydev completions top priority (see `:h blink.cmp`)
+                        score_offset = 100,
+                    },
+                },
+            },
+        })
+    end,
 })
