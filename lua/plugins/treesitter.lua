2e99e68a36fe47ee2685495adf8eb319336c47b2
Bradley Wood <52896759+BradleyLWood@users.noreplay.github.com>
2026-09-01 11:29:27 -0600

    adding treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,30 @@
-vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", name = "treesitter" } })
+vim.pack.add({
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter', name = 'treesitter' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter-context', name = 'treesitter-context' },
+})
 
-require("nvim-treesitter").setup({
+require('treesitter-context').setup()
+
+require('nvim-treesitter').setup({
 	highlight = { enable = true },
 	indent = { enable = true },
 })
 
-require("nvim-treesitter").install({
-	"bash",
-	"html",
-	"svelte",
-	"latex",
-	"javascript",
-	"json",
-	"lua",
-	"markdown",
-	"markdown_inline",
-	"query",
-	"regex",
-	"tsx",
-	"typescript",
-	"python",
-	"vim",
-	"yaml",
+require('nvim-treesitter').install({
+	'bash',
+	'html',
+	'svelte',
+	'latex',
+	'javascript',
+	'json',
+	'lua',
+	'markdown',
+	'markdown_inline',
+	'query',
+	'regex',
+	'tsx',
+	'typescript',
+	'python',
+	'vim',
+	'yaml',
 })
